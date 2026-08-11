cc36a3e54ed287d55ec8104e2995d1d56b4a2a49
Balla Thanuja <balla.thanuja@coforge.com>
2026-08-11 11:15:21 +0530

    Added Lab 12 Object Repository from .properties file

---
+++ ObjectRepositoryPropertiesLab12/src/test/java/package1/Repository_readfromproperties.java
@@ -0,0 +1,31 @@
+package package1;
+
+import java.io.FileInputStream;
+import java.util.Properties;
+
+public class Repository_readfromproperties {
+	static Properties prob = new Properties();
+
+	static String projectpath = System.getProperty("user.dir");
+    static {
+    	try {
+			String path = projectpath + "\\objectRepository.properties";
+
+			FileInputStream fis = new FileInputStream(path);
+
+			prob.load(fis);
+			fis.close();
+
+		} catch (Exception e) {
+			System.out.println(e.getMessage());
+
+		}
+    }
+	public static String getlocator(String key)
+
+	{
+
+		return prob.getProperty(key);
+	}
+
+}
---
+++ ObjectRepositoryPropertiesLab12/src/test/java/package1/SearchTest.java
@@ -0,0 +1,24 @@
+package package1;
+
+import org.testng.Assert;
+import org.testng.Reporter;
+import org.testng.annotations.DataProvider;
+import org.testng.annotations.Test;
+
+public class SearchTest extends BaseTest {
+	@Test(dataProvider = "searchData")
+	public void searchFlowTest(String firstSearch, String secondSearch) {
+		POM_properties pom = new POM_properties(driver);
+		pom.searchText(firstSearch, secondSearch);
+		String actualHeading = pom.resultPageHeading();
+		Reporter.log("Searching " + secondSearch);
+		Assert.assertEquals(actualHeading, "Search - " + secondSearch);
+	}
+
+	@DataProvider(name = "searchData")
+	public Object[][] searchData() {
+
+		return new Object[][] { { "Mobile", "Monitors" }, { "Laptop", "Phones" }, { "Camera", "Cameras" } };
+	}
+
+}
